3d418d8580f27bcb3e7ff38c44b99316a45985b2
aayushkri20005 <chhatrapatiaayush2003@gmail.com>
2025-04-05 02:02:52 +0530

    2ndlastjs

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const modelselect=document.getElementById("modelselect");
 const countselect=document.getElementById("countselect");
 const ratioselect=document.getElementById("ratioselect");
 const gridgallery=document.querySelector(".gallery-grid");
-const API_KEY="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"
+//const API_KEY="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"
 
 const examplepromt=[
 "A futuristic city floating in the clouds, with neon lights and flying cars, cyberpunk style.",
@@ -20,7 +20,7 @@ const examplepromt=[
 "A massive dragon made of constellations flying across a twilight sky."
 ];
 const getimagedime=(aspectratio)=>{
-    
+
 }
 const generateimage=async(selsectedmodel,imagecount,aspectratio,promttext)=>{
 const modelurl=`https://api-inference.huggingface.co/models/${selsectedmodel}`;
